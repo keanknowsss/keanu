8c912e89691987aa58f781dd8d1035acc0fab1ac
keanknowsss <solomon.keanu.sk@gmail.com>
2025-11-04 06:12:35 +0800

    add layout for project section and screenshots for each project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import Introduction from "./components/Introduction";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <Introduction />
       <Skills />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,163 @@
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <div className="projects-content">
+        <h2 className="description">
+          <span className="text-secondary-dark">From Concept to Code</span>
+          <br />a few highlights of my work so far.
+        </h2>
+        <ul className="projects-list">
+          <li>
+            <div>
+                <div className="img-container">
+                  <img src="https://picsum.photos/300/300?random=1" className="w-full h-full" alt="careva" />
+                </div>
+                <div className="mt-3 flex flex-col gap-2 ps-0.5">
+                    <h3 className="project-title">CAREVA</h3>
+                    <p className="project-description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nobis? Nostrum
+                      explicabo ipsam deserunt quos repudiandae ducimus dolore repellat ratione!
+                    </p>
+                </div>
+                <ul className="tags">
+                  <li className="fe">Flutter</li>
+                  <li className="be">Laravel</li>
+                  <li className="db">MySQL</li>
+                </ul>
+            </div>
+            <div className="view-btn-container">
+                <label htmlFor="view-project-1">View Details</label>
+                <button id="view-project-1"><FaArrowUpRightFromSquare /></button>
+            </div>
+          </li>
+          <li>
+            <div>
+                <div className="img-container">
+                  <img src="https://picsum.photos/300/300?random=2" className="w-full h-full" alt="JokeJoke" />
+                </div>
+                <div className="mt-3 flex flex-col gap-2 ps-0.5">
+                    <h3 className="project-title">Jokejoke</h3>
+                    <p className="project-description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nobis? Nostrum
+                      explicabo ipsam deserunt quos repudiandae ducimus dolore repellat ratione!
+                    </p>
+                </div>
+                <ul className="tags">
+                  <li className="be">Laravel Livewire</li>
+                  <li className="fe">Alpine.js</li>
+                  <li className="fe">Tailwindcss</li>
+                  <li className="fe">CSS</li>
+                  <li className="db">MySQL</li>
+                </ul>
+            </div>
+            <div className="view-btn-container">
+                <label htmlFor="view-project-2">View Details</label>
+                <button id="view-project-2"><FaArrowUpRightFromSquare /></button>
+            </div>
+          </li>
+          <li>
+            <div>
+                <div className="img-container">
+                  <img src="https://picsum.photos/300/300?random=3" className="w-full h-full" alt="Resu.me" />
+                </div>
+                <div className="mt-3 flex flex-col gap-2 ps-0.5">
+                    <h3 className="project-title">Resu.me</h3>
+                    <p className="project-description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nobis? Nostrum
+                      explicabo ipsam deserunt quos repudiandae ducimus dolore repellat ratione!
+                    </p>
+                </div>
+                <ul className="tags">
+                  <li className="fe">React.js</li>
+                  <li className="fe">Typescript</li>
+                  <li className="fe">Shadcn UI</li>
+                  <li className="fe">Tailwindcss</li>
+                  <li className="fe">CSS</li>
+                </ul>
+            </div>
+            <div className="view-btn-container">
+                <label htmlFor="view-project-3">View Details</label>
+                <button id="view-project-3"><FaArrowUpRightFromSquare /></button>
+            </div>
+          </li>
+          <li>
+            <div>
+                <div className="img-container">
+                  <img src="https://picsum.photos/300/300?random=4" className="w-full h-full" alt="The Keeper" />
+                </div>
+                <div className="mt-3 flex flex-col gap-2 ps-0.5">
+                    <h3 className="project-title">The Keeper</h3>
+                    <p className="project-description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nobis? Nostrum
+                      explicabo ipsam deserunt quos repudiandae ducimus dolore repellat ratione!
+                    </p>
+                </div>
+                <ul className="tags">
+                  <li className="be">Node.js</li>
+                  <li className="be">Express.js</li>
+                  <li className="fe">EJS</li>
+                  <li className="fe">CSS</li>
+                  <li className="db">Postgresql</li>
+                </ul>
+            </div>
+            <div className="view-btn-container">
+                <label htmlFor="view-project-4">View Details</label>
+                <button id="view-project-4"><FaArrowUpRightFromSquare /></button>
+            </div>
+          </li>
+          <li>
+            <div>
+                <div className="img-container">
+                  <img src="https://picsum.photos/300/300?random=5" className="w-full h-full" alt="ToDo.me" />
+                </div>
+                <div className="mt-3 flex flex-col gap-2 ps-0.5">
+                    <h3 className="project-title">ToDo.me</h3>
+                    <p className="project-description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nobis? Nostrum
+                      explicabo ipsam deserunt quos repudiandae ducimus dolore repellat ratione!
+                    </p>
+                </div>
+                <ul className="tags">
+                  <li className="be">Node.js</li>
+                  <li className="be">Express.js</li>
+                  <li className="fe">EJS</li>
+                  <li className="fe">CSS</li>
+                </ul>
+            </div>
+            <div className="view-btn-container">
+                <label htmlFor="view-project-5">View Details</label>
+                <button id="view-project-5"><FaArrowUpRightFromSquare /></button>
+            </div>
+          </li>
+          <li>
+            <div>
+                <div className="img-container">
+                  <img src="https://picsum.photos/300/300?random=6" className="w-full h-full" alt="Pixel Positions" />
+                </div>
+                <div className="mt-3 flex flex-col gap-2 ps-0.5">
+                    <h3 className="project-title">Pixel Positions</h3>
+                    <p className="project-description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nobis? Nostrum
+                      explicabo ipsam deserunt quos repudiandae ducimus dolore repellat ratione!
+                    </p>
+                </div>
+                <ul className="tags">
+                  <li className="be">Laravel</li>
+                  <li className="fe">Tailwindcss</li>
+                  <li className="db">SQLite</li>
+                </ul>
+            </div>
+            <div className="view-btn-container">
+                <label htmlFor="view-project-6">View Details</label>
+                <button id="view-project-6"><FaArrowUpRightFromSquare /></button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default Projects;
